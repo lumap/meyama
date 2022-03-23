351a6191f1b1b781445ecd96f0f90933c532b744
lumap <lumapbk@gmail.com>
2022-03-23 14:57:00 +0100

    fix status ig + startup time

--- mod.ts
+++ mod.ts
@@ -1,7 +1,26 @@
 import * as deps from "./deps.ts";
 
+
+console.log('Bot starting...')
+const time = Date.now();
+
+
+function status() {
+    bot.helpers.editBotStatus({
+        status: "idle",
+        activities: [
+            {
+                name: "discord messages",
+                type: deps.ActivityTypes.Watching,
+                createdAt: 1
+            }
+        ]
+    })
+}
+
+
 const bot = deps.createBot({
-    intents: ["GuildMessages", "Guilds", "GuildMessages","DirectMessages"],
+    intents: ["GuildMessages", "Guilds", "GuildMessages", "DirectMessages"],
     token: deps.config.token,
     botId: deps.config.botId,
     events: deps.createEventHandlers({
@@ -10,37 +29,24 @@ const bot = deps.createBot({
             if (message.content.includes("https://media.discordapp.net") && checkEndExtension(message.content.toLowerCase())) {
                 try {
                     await bot.helpers.deleteMessage(message.channelId, message.id, "Replacing media.discordapp.net with cdn.discordapp.com");
-                    await bot.helpers.sendMessage(message.channelId, { content: `Sent by <@${message.authorId}>\n` + message.content.replaceAll("https://media.discordapp.net", "https://cdn.discordapp.com") })
+                    await bot.helpers.sendMessage(message.channelId, { content: `From<@${message.authorId}>\n> ` + message.content.replaceAll("https://media.discordapp.net", "https://cdn.discordapp.com") })
                 } catch {
                     "1";
                 }
             }
         },
         ready() {
-            console.log("ready or something idk")
+            console.log(`Ready! Took ${Date.now() - time}ms`)
+            status()
+            setInterval(status, 1800000)
         }
     }),
 })
 
 deps.startBot(bot)
 
-function status() {
-    bot.helpers.editBotStatus({
-        status: "idle",
-        activities: [
-            {
-                name: "war against russia",
-                type: deps.ActivityTypes.Watching,
-                createdAt: 1
-            }
-        ]
-    })
-}
-status()
-setInterval(status,1800000)
-
 function checkEndExtension(str: string): boolean {
-    const exts = [".mov", ".mp4", ".flv", ".m4a",".mkv","avi"];
+    const exts = [".mov", ".mp4", ".flv", ".m4a", ".mkv", "avi"];
     let res = false;
     for (let i = 0; i < exts.length && res == false; i++) {
         res = str.endsWith(exts[i])
